51c52a235c7a15d2a63fb275b973a22ade2634ba
reifschneider-helen <reifschneider.helen@gmail.com>
2025-05-26 15:55:34 +0200

    Improve code formatting and unify quotes style

--- src/components/todos/todosSlice.js
+++ src/components/todos/todosSlice.js
@@ -1,43 +1,44 @@
-import { createSlice, isPending, isRejected } from "@reduxjs/toolkit";
-import { fetchTodos, addTodo, toggleTodo, deleteTodo } from "./todosThunks";
+import { createSlice, isPending, isRejected } from '@reduxjs/toolkit';
+import { fetchTodos, addTodo, toggleTodo, deleteTodo } from './todosThunks';
 
 const sortTodos = (todoArray) => {
   todoArray.sort((todo1, todo2) => {
     if (todo1.done === todo2.done) return 0;
+
     return todo1.done ? 1 : -1;
   });
 };
 
 const initialState = {
   todos: [],
-  status: "idle",
+  status: 'idle',
   error: null,
 };
 
 const todosSlice = createSlice({
-  name: "todos",
+  name: 'todos',
   initialState,
   extraReducers: (builder) => {
     builder
       .addCase(fetchTodos.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.todos = action.payload;
         sortTodos(state.todos);
       })
       .addCase(fetchTodos.pending, (state) => {
-        state.status = "loading";
+        state.status = 'loading';
       })
       .addCase(fetchTodos.rejected, (state, action) => {
-        state.status = "failed";
+        state.status = 'failed';
         state.error = action.error.message;
       })
       .addCase(addTodo.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.todos.push(action.payload);
         sortTodos(state.todos);
       })
       .addCase(toggleTodo.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.todos.map((todo) => {
           if (todo.id === action.payload.id) {
             todo.done = !todo.done;
@@ -46,7 +47,7 @@ const todosSlice = createSlice({
         sortTodos(state.todos);
       })
       .addCase(deleteTodo.fulfilled, (state, action) => {
-        state.status = "succeeded";
+        state.status = 'succeeded';
         state.todos = state.todos.filter((todo) => todo.id !== action.payload);
       });
   },
--- src/components/todos/todosThunks.js
+++ src/components/todos/todosThunks.js
@@ -1,34 +1,38 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-export const fetchTodos = createAsyncThunk("todos/fetchTodos", async () => {
-  const response = await fetch("http://localhost:3001/api/todos");
+export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
+  const response = await fetch('http://localhost:3001/api/todos');
   const data = await response.json();
+
   return data;
 });
 
-export const addTodo = createAsyncThunk("todos/addTodo", async (text) => {
-  const response = await fetch("http://localhost:3001/api/todos", {
-    method: "POST",
+export const addTodo = createAsyncThunk('todos/addTodo', async (text) => {
+  const response = await fetch('http://localhost:3001/api/todos', {
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ text }),
   });
   const newTodo = await response.json();
+
   return newTodo;
 });
 
-export const toggleTodo = createAsyncThunk("todos/toggleTodo", async (id) => {
+export const toggleTodo = createAsyncThunk('todos/toggleTodo', async (id) => {
   const response = await fetch(`http://localhost:3001/api/todos/${id}`, {
-    method: "PATCH",
+    method: 'PATCH',
   });
   const todo = await response.json();
+
   return todo;
 });
 
-export const deleteTodo = createAsyncThunk("todos/deleteTodo", async (id) => {
+export const deleteTodo = createAsyncThunk('todos/deleteTodo', async (id) => {
   await fetch(`http://localhost:3001/api/todos/${id}`, {
-    method: "DELETE",
+    method: 'DELETE',
   });
+
   return id;
 });
