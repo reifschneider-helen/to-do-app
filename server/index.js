e77af7e415cab7262426b7b7715a8e33b99c8199
reifschneider-helen <reifschneider.helen@gmail.com>
2025-05-27 16:09:56 +0200

    Add mongoDB as backend

--- server/index.js
+++ server/index.js
@@ -1,80 +1,67 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const fs = require('fs');
-const path = require('path');
+const connectDB = require('./db');
+const Todo = require('./models/todo');
 
 const server = express();
-
 server.use(cors({ origin: 'http://localhost:3000' }));
 server.use(express.json());
 
-const filePath = path.join(__dirname, 'todos.json');
+connectDB();
 
-function loadTodos() {
-  const data = fs.readFileSync(filePath, 'utf-8');
-  
-  return JSON.parse(data);
-}
-
-function saveTodos(todos) {
-  fs.writeFileSync(filePath, JSON.stringify(todos, null, 2));
-}
-
-server.get('/api/todos', (req, res) => {
-  const todos = loadTodos();
-
-  res.status(200).json(todos);
+server.get('/api/todos', async (req, res) => {
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (error) {
+    res.status(500).json({ error: 'Error while loadind Todos' });
+  }
 });
 
-server.post('/api/todos', (req, res) => {
+server.post('/api/todos', async (req, res) => {
   const { text } = req.body;
 
   if (!text || text.trim() === '') {
     return res.status(400).json({ error: 'Invalid text for new todo' });
   }
 
-  const todos = loadTodos();
-  const newTodo = {
-    id: Date.now(),
-    text: text.trim(),
-    done: req.body.done ?? false,
-  };
-
-  todos.push(newTodo);
-  saveTodos(todos);
-
-  res.status(201).json(newTodo);
-});
-
-server.patch('/api/todos/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const todos = loadTodos();
-
-  const todo = todos.find((todo) => todo.id === id);
-
-  if (todo) {
-    todo.done = !todo.done;
-    saveTodos(todos);
-
-    res.status(200).json(todo);
-  } else {
-    res.status(404).json({ error: 'Todo does not exist' });
+  try {
+    const newTodo = new Todo({ text: text.trim() });
+    await newTodo.save();
+    res.status(201).json(newTodo);
+  } catch (error) {
+    return res.status(500).json({ error: 'Error while saving Todos' });
   }
 });
 
-server.delete('/api/todos/:id', (req, res) => {
-  const id = Number(req.params.id);
-  const todos = loadTodos();
+server.patch('/api/todos/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const todo = await Todo.findById(id);
+    if (!todo) {
+      return res.status(404).json({ error: 'Todo is not found' });
+    }
 
-  const todoExists = todos.some((todo) => todo.id === id);
+    todo.done = !todo.done;
+    await todo.save();
 
-  if (todoExists) {
-    const newTodos = todos.filter((todo) => todo.id !== id);
-    saveTodos(newTodos);
+    res.status(200).json(todo);
+  } catch (error) {
+    res.status(500).json({ error: 'Error while updating' });
+  }
+});
 
-    res.status(200).json(newTodos);
-  } else {
-    res.status(404).json({ error: 'Todo does not exist' });
+server.delete('/api/todos/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const deleted = await Todo.findByIdAndDelete(id);
+    if (!deleted) {
+      return res.status(404).json({ error: 'Todo is not found' });
+    }
+    res.status(200).json(deleted);
+  } catch (error) {
+    res.status(500).json({ error: 'Error while deleting' });
   }
 });
 
